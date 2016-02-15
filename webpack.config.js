7b17c4916d2aa0b2b5a27fa280b298bbae354ec9
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-15 15:02:34 -0600

    adding redux app

--- webpack.config.js
+++ webpack.config.js
@@ -7,38 +7,36 @@ var HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   devtool: 'eval-source-map',
   entry: [
-    'webpack-hot-middleware/client?reload=true',
-    path.join(__dirname, 'app/main.js')
+  'webpack-hot-middleware/client?reload=true',
+  path.join(__dirname, 'app/index.js')
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name].js',
     publicPath: '/'
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new webpack.HotModuleReplacementPlugin(),
-    new webpack.NoErrorsPlugin(),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('development')
-    })
-  ],
-  module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
-    }, {
-      test: /\.css$/,
-      loader: 'style!css?modules&localIdentName=[name]---[local]---[hash:base64:5]'
-    }]
-  }
+},
+plugins: [
+new HtmlWebpackPlugin({
+  template: 'app/index.html',
+  inject: 'body',
+  filename: 'index.html'
+}),
+new webpack.optimize.OccurenceOrderPlugin(),
+new webpack.HotModuleReplacementPlugin(),
+new webpack.NoErrorsPlugin(),
+new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify('development')
+})
+],
+module: {
+    loaders: [
+        { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' }, 
+        { test: /\.css$/, loaders: ['style-loader', 'css-loader?sourceMap'] },
+        { test: /\.scss$/, loaders: ['style-loader', 'css-loader?sourceMap!sass-loader?sourceMap'] },
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'url-loader?limit=10000&minetype=application/font-woff' },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: 'file-loader' },
+        { test: /\.(jpg|jpeg|gif|png|ico)$/, exclude: /node_modules/, loader: 'url-loader?limit=8192' },
+        { test: /\.(ico)$/, loader: 'static-loader' }
+    ]
+}
 };
--- webpack.production.config.js
+++ webpack.production.config.js
@@ -7,44 +7,42 @@ var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
   entry: [
-    path.join(__dirname, 'app/main.js')
+  path.join(__dirname, 'app/index.js')
   ],
   output: {
     path: path.join(__dirname, '/dist/'),
     filename: '[name]-[hash].min.js'
-  },
-  plugins: [
-    new webpack.optimize.OccurenceOrderPlugin(),
-    new HtmlWebpackPlugin({
-      template: 'app/index.html',
-      inject: 'body',
-      filename: 'index.html'
-    }),
-    new ExtractTextPlugin('[name]-[hash].min.css'),
-    new webpack.optimize.UglifyJsPlugin({
-      compressor: {
-        warnings: false,
-        screw_ie8: true
-      }
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-    })
-  ],
-  module: {
-    loaders: [{
-      test: /\.js?$/,
-      exclude: /node_modules/,
-      loader: 'babel'
-    }, {
-      test: /\.json?$/,
-      loader: 'json'
-    }, {
-      test: /\.css$/,
-      loader: ExtractTextPlugin.extract('style', 'css?modules&localIdentName=[name]---[local]---[hash:base64:5]!postcss')
-    }]
-  },
-  postcss: [
-    require('autoprefixer')
-  ]
+},
+plugins: [
+new webpack.optimize.OccurenceOrderPlugin(),
+new HtmlWebpackPlugin({
+  template: 'app/index.html',
+  inject: 'body',
+  filename: 'index.html'
+}),
+new ExtractTextPlugin('[name]-[hash].min.css'),
+new webpack.optimize.UglifyJsPlugin({
+  compressor: {
+    warnings: false,
+    screw_ie8: true
+}
+}),
+new webpack.DefinePlugin({
+  'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+})
+],
+module: {
+    loaders: [
+        { test: /\.js?$/, exclude: /node_modules/, loader: 'babel' }, 
+        { test: /\.css$/, loaders: ["style-loader", "css-loader?sourceMap"] },
+        { test: /\.scss$/, loaders: ["style-loader", "css-loader?sourceMap!sass-loader?sourceMap"] },
+        { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+        { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" },
+        { test: /\.(jpg|jpeg|gif|png|ico)$/, exclude: /node_modules/, loader: 'url-loader?limit=8192' },
+        { test: /\.(ico)$/, loader: "static-loader" }
+    ]
+},
+postcss: [
+require('autoprefixer')
+]
 };
