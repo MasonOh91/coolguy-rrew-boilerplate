8d3754fd9fa6ef55ba26faecc13c0137f6729c55
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-21 15:44:42 -0600

    testing

--- server.js
+++ server.js
@@ -6,31 +6,31 @@ var isDeveloping = process.env.NODE_ENV !== 'production';
 var port = isDeveloping ? 3000 : process.env.PORT;
 var app = express();
 
-if (isDeveloping) {
-  var webpack = require('webpack');
-  var config = require('./webpack.config');
+// if (isDeveloping) {
+//   var webpack = require('webpack');
+//   var config = require('./webpack.config');
 
-  var webpackMiddleware = require('webpack-dev-middleware');
-  var webpackHotMiddleware = require('webpack-hot-middleware');
-  var compiler = webpack(config);
-  var middleware = webpackMiddleware(compiler, {
-      publicPath: config.output.publicPath,
-      contentBase: 'src',
-      stats: {
-        colors: true,
-        hash: false,
-        timings: true,
-        chunks: false,
-        chunkModules: false,
-        modules: false
-      }
-    });
+//   var webpackMiddleware = require('webpack-dev-middleware');
+//   var webpackHotMiddleware = require('webpack-hot-middleware');
+//   var compiler = webpack(config);
+//   var middleware = webpackMiddleware(compiler, {
+//       publicPath: config.output.publicPath,
+//       contentBase: 'src',
+//       stats: {
+//         colors: true,
+//         hash: false,
+//         timings: true,
+//         chunks: false,
+//         chunkModules: false,
+//         modules: false
+//       }
+//     });
 
-    app.use(middleware);
-    app.use(webpackHotMiddleware(compiler));
-  } else {
+//     app.use(middleware);
+//     app.use(webpackHotMiddleware(compiler));
+//   } else {
     app.use(express.static(__dirname + '/dist'));
-  }
+  // }
 
   app.get('*', function response(req, res) {
     res.sendFile(path.join(__dirname, 'dist/index.html'));
