dc3460717a03cfe736b7985981cf2db7bdf4bbef
Mason O'Mara <mason.omara@shoutlet.com>
2016-02-21 21:42:36 -0600

    edit and complete functions and tests

--- app/reducers/todos.js
+++ app/reducers/todos.js
@@ -8,22 +8,37 @@ const initial_state = [{
 
 export default function reducer(state = initial_state, action) {
     switch (action.type) {
-    case action_types.ADD_TODO:
-        return [{
-            id: state.reduce((maxId, state_item) => Math.max(state_item.id, maxId), -1) + 1,
-            text: action.text
-        },
-        ...state
+        case action_types.ADD_TODO:
+        return [
+        ...state,
+        {id: state.reduce((maxId, state_item) => Math.max(state_item.id, maxId), -1) + 1,
+            text: action.text,
+            completed: false
+        }
         ];
-    case action_types.DELETE_TODO:
-        return state.filter(todo => {
-            return todo.id !== action.id;
-        });
-    case action_types.EDIT_TODO:
-        return state;
-    case action_types.COMPLETE_TODO:
-        return state;
-    default:
-        return state;
+        case action_types.DELETE_TODO:
+            return state.filter(todo => {
+                return todo.id !== action.id;
+            });
+        case action_types.EDIT_TODO:
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return Object.assign({}, todo, {
+                        text: action.text
+                    });
+                }
+                return todo;
+            });
+        case action_types.COMPLETE_TODO:
+            return state.map(todo => {
+                if (todo.id === action.id) {
+                    return Object.assign({}, todo, {
+                        completed: !todo.completed
+                    });
+                }
+                return todo;
+            });
+        default:
+            return state;
     }
 }
---
+++ test/reducers/todos.test.js
@@ -0,0 +1,29 @@
+import todos from '../../app/reducers/todos';
+
+describe('todos reducers tests', () => {
+    const initial_state = [{id: 1, text: 'todo', completed: false}];
+
+    it('should add a todo to the todo list', () => {
+        expect(todos(initial_state, {type: 'ADD_TODO', text: 'todo2'}))
+            .toEqual([{id: 1, text: 'todo', completed: false}, {id: 2, text: 'todo2', completed: false}]);
+    });
+
+    it('remove a todo from the todo list', () => {
+        expect(todos(initial_state, {type: 'DELETE_TODO', id: 1}))
+            .toEqual([]);
+    });
+
+    it('edit a todo from the todo list', () => {
+        expect(todos(initial_state, {type: 'EDIT_TODO', id: 1, text: 'test success'}))
+            .toEqual([{id: 1, text: 'test success', completed: false}]);
+    });
+
+    it('completed a todo from the todo list', () => {
+        const completed_temp = todos(initial_state, {type: 'COMPLETE_TODO', id: 1});
+        expect(completed_temp)
+            .toEqual([{id: 1, text: 'todo', completed: true}]);
+
+        expect(todos(completed_temp, {type: 'COMPLETE_TODO', id: 1, text: 'todo'}))
+            .toEqual([{id: 1, text: 'todo', completed: false}]);
+    });
+});
